bd0434a30b93476a096e1c0e79f3869f7c8ff35f
DeenCollegetn <147496406+DeenCollegetn@users.noreply.github.com>
2023-10-10 15:26:40 +0530

    Update index.html

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
+<html>
+	<head>
+		<title>
+			deencollege
+		</title>
+	</head>
 
-	<?php include('common/header.php') ?>
-<img src="images/clg.jpg" class="logo-image" width="1600" height="630">
+<body><img src="images/clg.jpg" class="logo-image" width="1600" height="630"></body></html>
+	
+
 
   
   
   
-	<?php include('common/footer.php') ?>
+	
 
